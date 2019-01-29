f106cfa3d112fa2c3357762e144b8b7864491e85
MrLucasFischer <lucasfischerpt@gmail.com>
2019-01-29 18:02:28 +0000

    Wroking on retrieving top passage

--- src/main/java/Server.java
+++ src/main/java/Server.java
@@ -1,7 +1,12 @@
 import edu.unh.cs.treccar_v2.Data;
+import edu.unh.cs.treccar_v2.read_data.DeserializeData;
+import org.apache.lucene.analysis.en.EnglishAnalyzer;
+import org.apache.lucene.document.Document;
 import org.apache.lucene.index.DirectoryReader;
 import org.apache.lucene.index.IndexReader;
 import org.apache.lucene.search.IndexSearcher;
+import org.apache.lucene.search.ScoreDoc;
+import org.apache.lucene.search.TopDocs;
 import org.apache.lucene.search.similarities.BM25Similarity;
 import org.apache.lucene.search.similarities.LMDirichletSimilarity;
 import org.apache.lucene.store.Directory;
@@ -9,9 +14,12 @@ import org.apache.lucene.store.FSDirectory;
 import org.jetbrains.annotations.NotNull;
 import org.json.JSONObject;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.nio.file.FileSystems;
 import java.nio.file.Path;
+import java.util.HashSet;
 import java.util.List;
 
 import static spark.Spark.*;
@@ -24,31 +32,32 @@ public class Server {
 
         System.setProperty("file.encoding", "UTF-8");
 
-        if (args.length != 1) {
+        if (args.length != 2) {
             usage();
         }
 
         port(5901);
-        setUpIndex(args[0]);
-        setUpEndPoints();
+        setUpIndex(args[1]);
+        setUpEndPoints(args[2]);
     }
 
     /**
      * Error message if not command line parameters not passed
      */
     private static void usage() {
-        System.out.println("Command line parameters: indexPath");
+        System.out.println("Command line parameters: paragraphsCBOR indexPath");
         System.exit(-1);
     }
 
     /**
      * Sets up the endpoints for the server
      */
-    private static void setUpEndPoints() {
+    private static void setUpEndPoints(String cborFilePath) {
 
         get("/query", "application/json", (req, res) -> {
 
             JSONObject json = new JSONObject(req.body());
+
             if ("bm25".equals(json.getString("algo"))) {
                 float k1 = json.getFloat("k1");
                 float b = json.getFloat("b");
@@ -57,8 +66,52 @@ public class Server {
                 searcher.setSimilarity(new LMDirichletSimilarity(json.getFloat("mu")));
             }
 
+            getTopPassage(cborFilePath);
+
+
             return "algo: " + json.getString("algo") + "\nk1: " + json.getFloat("k1") + "\nb: " + json.getFloat("b") + "\nquery: " + json.getString("query") + "\nsearcher: " + searcher.toString();
         });
+    }
+
+    private static String getTopPassage(String cborFilePath) {
+        final QueryBuilder queryBuilder = new QueryBuilder(new EnglishAnalyzer());
+        try {
+            final FileInputStream fileInputStream3 = new FileInputStream(new File(cborFilePath));
+            //TODO START FROM HERE
+            for (Data.Page page : DeserializeData.iterableAnnotations(fileInputStream3)) {
+
+                for (List<Data.Section> sectionPath : page.flatSectionPaths()) {
+                    final String queryId = Data.sectionPathId(page.getPageId(), sectionPath); //Get QueryID
+
+//                    sectionPath.forEach(section -> System.out.println(section.getHeading()));
+
+                    String queryStr = buildSectionQueryStr(page, sectionPath);  //Get queryString to search
+//                    System.out.println(queryBuilder.toQuery(queryStr));
+                    TopDocs tops = searcher.search(queryBuilder.toQuery(queryStr), 100);//Get 100 docs for the provided query
+
+                    ScoreDoc[] scoreDoc = tops.scoreDocs;
+                    HashSet<Object> seen = new HashSet<>(100);
+
+                    for (int i = 0; i < scoreDoc.length; i++) {
+
+                        ScoreDoc score = scoreDoc[i];
+                        final Document doc = searcher.doc(score.doc); // to access stored content
+                        final String paragraphid = doc.getField("paragraphid").stringValue();
+                        final float searchScore = score.score;
+                        final int searchRank = i + 1;
+
+                        if (!seen.contains(paragraphid)) {
+                            System.out.println(queryId + " Q0 " + paragraphid + " " + searchRank + " " + searchScore + " Lucene-BM25");
+                            seen.add(paragraphid);
+                        }
+                    }
+
+                }
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
 
