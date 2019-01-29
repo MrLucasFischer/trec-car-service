19ea9112c258a80bd4a1b84bcde3a118f1eaef84
MrLucasFischer <lucasfischerpt@gmail.com>
2019-01-29 18:06:48 +0000

    Wroking on retrieving top passage

--- src/main/java/Server.java
+++ src/main/java/Server.java
@@ -32,27 +32,27 @@ public class Server {
 
         System.setProperty("file.encoding", "UTF-8");
 
-        if (args.length != 2) {
+        if (args.length != 1) {
             usage();
         }
 
         port(5901);
-        setUpIndex(args[1]);
-        setUpEndPoints(args[2]);
+        setUpIndex(args[0]);
+        setUpEndPoints();
     }
 
     /**
      * Error message if not command line parameters not passed
      */
     private static void usage() {
-        System.out.println("Command line parameters: paragraphsCBOR indexPath");
+        System.out.println("Command line parameters: indexPath");
         System.exit(-1);
     }
 
     /**
      * Sets up the endpoints for the server
      */
-    private static void setUpEndPoints(String cborFilePath) {
+    private static void setUpEndPoints() {
 
         get("/query", "application/json", (req, res) -> {
 
@@ -66,52 +66,22 @@ public class Server {
                 searcher.setSimilarity(new LMDirichletSimilarity(json.getFloat("mu")));
             }
 
-            getTopPassage(cborFilePath);
-
-
-            return "algo: " + json.getString("algo") + "\nk1: " + json.getFloat("k1") + "\nb: " + json.getFloat("b") + "\nquery: " + json.getString("query") + "\nsearcher: " + searcher.toString();
+            return getTopPassage(json.getString("query"));
         });
     }
 
-    private static String getTopPassage(String cborFilePath) {
-        final QueryBuilder queryBuilder = new QueryBuilder(new EnglishAnalyzer());
+    private static String getTopPassage(String userQuery) {
+        QueryBuilder queryBuilder = new QueryBuilder(new EnglishAnalyzer());
         try {
-            final FileInputStream fileInputStream3 = new FileInputStream(new File(cborFilePath));
-            //TODO START FROM HERE
-            for (Data.Page page : DeserializeData.iterableAnnotations(fileInputStream3)) {
-
-                for (List<Data.Section> sectionPath : page.flatSectionPaths()) {
-                    final String queryId = Data.sectionPathId(page.getPageId(), sectionPath); //Get QueryID
-
-//                    sectionPath.forEach(section -> System.out.println(section.getHeading()));
-
-                    String queryStr = buildSectionQueryStr(page, sectionPath);  //Get queryString to search
-//                    System.out.println(queryBuilder.toQuery(queryStr));
-                    TopDocs tops = searcher.search(queryBuilder.toQuery(queryStr), 100);//Get 100 docs for the provided query
-
-                    ScoreDoc[] scoreDoc = tops.scoreDocs;
-                    HashSet<Object> seen = new HashSet<>(100);
-
-                    for (int i = 0; i < scoreDoc.length; i++) {
-
-                        ScoreDoc score = scoreDoc[i];
-                        final Document doc = searcher.doc(score.doc); // to access stored content
-                        final String paragraphid = doc.getField("paragraphid").stringValue();
-                        final float searchScore = score.score;
-                        final int searchRank = i + 1;
-
-                        if (!seen.contains(paragraphid)) {
-                            System.out.println(queryId + " Q0 " + paragraphid + " " + searchRank + " " + searchScore + " Lucene-BM25");
-                            seen.add(paragraphid);
-                        }
-                    }
-
-                }
-            }
+            TopDocs tops = searcher.search(queryBuilder.toQuery(userQuery), 100);
+            ScoreDoc[] scoreDoc = tops.scoreDocs;
+            HashSet<Object> seen = new HashSet<>(100);
+//            //TODO START FROM HERE
 
         } catch (Exception e) {
             e.printStackTrace();
         }
+        return "TBD";
     }
 
 
