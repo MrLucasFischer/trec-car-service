06fb890a7522b85488cc7862a021830aeb2b2ee2
MrLucasFischer <lucasfischerpt@gmail.com>
2019-01-29 17:54:20 +0000

    refactoring code

--- src/main/java/Server.java
+++ src/main/java/Server.java
@@ -57,7 +57,7 @@ public class Server {
                 searcher.setSimilarity(new LMDirichletSimilarity(json.getFloat("mu")));
             }
 
-            return "algo: " + json.get("algorithm") + "\nk1: " + json.get("k1") + "\nb: " + json.get("b") + "\nquery: " + json.get("query") + "\nsearcher: " + searcher.toString();
+            return "algo: " + json.getString("algo") + "\nk1: " + json.getFloat("k1") + "\nb: " + json.getFloat("b") + "\nquery: " + json.getString("query") + "\nsearcher: " + searcher.toString();
         });
     }
 
